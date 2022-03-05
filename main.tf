4d34c0e93426ffbb7f809ac69e96d01b9eb8b6f4
Adriano Avelino <adriano-st777@hotmail.com>
2022-03-05 14:20:42 -0300

    adiciona recurso em região diferente

--- main.tf
+++ main.tf
@@ -13,6 +13,21 @@ provider "aws" {
   }
 }
 
+provider "aws" {
+  alias = "us_east-2"
+  region                      = "us-east-2"
+  access_key                  = "teste"
+  secret_key                  = "teste"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  profile                     = "localstack"
+  s3_use_path_style           = true
+  endpoints {
+    ec2 = "http://localhost:4566"
+  }
+}
+
 resource "aws_instance" "dev" {
   count         = 3
   ami           = "ami-026c8acd92718196b"
@@ -43,6 +58,17 @@ resource "aws_instance" "dev5" {
     Name = "dev5"
   }
   vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+}
+
+resource "aws_instance" "dev6" {
+  provider = aws.us_east-2
+  ami           = "ami-0d8f6eb4f641ef691"
+  instance_type = "t2.micro"
+  key_name      = "terraform-aws"
+  tags = {
+    Name = "dev6"
+  }
+  vpc_security_group_ids = ["${aws_security_group.acesso-ssh_us_east-2.id}"]
 }
 
 resource "aws_s3_bucket" "dev4" {
--- security_group.tf
+++ security_group.tf
@@ -14,3 +14,21 @@ resource "aws_security_group" "acesso-ssh" {
     Name = "ssh"
   }
 }
+
+resource "aws_security_group" "acesso-ssh_us_east-2" {
+  provider = aws.us_east-2
+  name        = "acesso-ssh"
+  description = "acesso-ssh"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["191.32.154.79/32"]
+  }
+  tags = {
+    Name = "ssh"
+  }
+}
